507022d7096fe2b089cbe882e68400e407b64e20
Richard Hanolt <rhanolt@gmail.com>
2020-05-22 01:05:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,9 +25,7 @@ class Song
   end
 
   def artist_name=(name)
-    new_artist = Artist.find_or_create_by_name(name)
-    self.artist = new_artist
-
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
 end
