13eb003a243c512cb2e8dd3e25ff365b5a809351
Richard Hanolt <rhanolt@gmail.com>
2020-05-22 02:18:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,11 +9,11 @@ class MP3Importer
 
   def files
     file_name = Dir.glob("#{path}/*.mp3")
-    file_name.collect{|file| (file.split("/"))[-1]}
+    @path = file_name.collect{|file| (file.split("/"))[-1]}
   end
 
   def import
-
+    Song.new_by_filename
   end
 
 end
