aae65952af33a4007d9c49c5774c20a806da12bb
Richard Hanolt <rhanolt@gmail.com>
2020-05-22 01:14:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,6 @@ class Song
     new_song_artist = parsed_file[0]
     new_song_name = parsed_file[1]
     new_song = Song.new(new_song_name)
-    binding.pry
     new_song.artist = new_song_artist
   end
 
